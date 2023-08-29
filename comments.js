8c2b82362af64222d7265ca41a1339db13d46ffa
Margot Foster <margot.foster@gmail.com>
2023-08-29 21:49:15 +0000

    git three

--- comments.js
+++ comments.js
@@ -1,39 +1,13 @@
-// create web server
-// 1. create a web server
-// 2. create a route (path) to the server
-// 3. create a callback function to handle the route
-// 4. start the server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var port = 3000;
-var comments = [
-  {
-    name: 'John',
-    comment: 'Hello World!'
-  }
-];
+// create web server 
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// import modules
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
-app.get('/comments', function(req, res) {
-  res.json(comments);
-});
-
-app.post('/comments', function(req, res) {
-  comments.push(req.body);
-  fs.writeFile('comments.json', JSON.stringify(comments, null, 2), function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('comments saved');
-    }
-  });
-  res.json(comments);
-});
-
-app.listen(port, function() {
-  console.log('server is running on port ' + port);
-});
+// set port
+const port = 3000;
+const hostname = 'localhost';
